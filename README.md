2dc007d955e92507150c1db60b93479cae285f67
Xcelerate <musxlabs@gmail.com>
2024-02-05 17:35:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Xcelerate
+
+🚀 This repository contains the core smart contracts for the Xcelerate Protocol.
+
+## Introduction
+
+The contracts directory contains the source code for all contracts.
+
+The docs directory contains documentation for all contracts, including:
+- Interface definitions
+- Deployment guides
+- Security audit reports
+
+The examples directory contains examples of how to use the contracts.
+
+
+## Bug bounty
+
+We are excited to launch our bug bounty program to encourage security researchers to find and report security vulnerabilities in Xcelerate contracts.
+
+Program Details:
+
+Anyone can submit security vulnerabilities found in Xcelerate contracts.
+We will reward vulnerability finders based on the severity of the vulnerability.
+
+Rewards can be up to $10,000 USD.
+
+
+## Contact Us
+
+If you have any questions or suggestions, please contact us.
+
+
+@Xcelerate Tech Team
---
+++ contracts/XBS.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+//  __  __        _                _       
+//  \ \/ /___ ___| | ___ _ __ __ _| |_ ___ 
+//   \  // __/ _ \ |/ _ \ '__/ _` | __/ _ \
+//   /  \ (_|  __/ |  __/ | | (_| | ||  __/
+//  /_/\_\___\___|_|\___|_|  \__,_|\__\___|
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title XBS Token Contract
+/// @notice Implements an ERC20 token with a cap, burnability, pausability, and ownership features
+contract XBS is
+    ERC20Burnable,
+    ERC20Permit,
+    ERC20Capped,
+    ERC20Pausable,
+    Ownable
+{
+    /// @notice Initializes the contract with a name, symbol, cap, and initial owner
+    constructor()
+        ERC20("Xcelerate", "XB")
+        ERC20Permit("Xcelerate")
+        ERC20Capped(423 * 1e6)
+        Ownable()
+    {}
+
+    /// @notice Allows the owner to pause all token transfers
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice Allows the owner to unpause the token transfers
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /// @notice Allows the owner to mint new tokens, up to the cap
+    /// @param to The address that will receive the minted tokens
+    /// @param amount The amount of tokens to mint
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    /// @dev Internal function to update state during transfers, respecting the cap and pausability
+    /// @param from The address sending the tokens
+    /// @param to The address receiving the tokens
+    /// @param value The amount of tokens being transferred
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Capped, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
